29b5ba64909011b4e18f5e25dda05905ee8c4b8a
Greg Griffith <griffith_g@hotmail.com>
2023-06-02 02:07:53 -0400

    add basic tech stack component

---
+++ app/components/TechStack.tsx
@@ -0,0 +1,27 @@
+
+export default function TechStack() {
+
+
+    return (
+        <main className="flex justify-between p-10 pl-20 pr-20">
+            <div className="space-y-2">
+                <p className="text-xl font-bold pb-2">Tech Stack</p>
+                <p className="text-xl">I have experience with the following technologies:</p>
+                <ul className="list-disc list-inside">
+                    <li>React</li>
+                    <li>Next.js</li>
+                    <li>Typescript</li>
+                    <li>Node.js</li>
+                    <li>Express</li>
+                    <li>PostgreSQL</li>
+                    <li>Python</li>
+                    <li>Java</li>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                </ul>
+            </div>
+        
+        </main>   
+    )
+}
