f3e0b1c6603b49cc596798994e980015c399f7e0
Greg Griffith <griffith_g@hotmail.com>
2023-06-03 23:37:48 -0400

    added font awesome icons to contact methods

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,9 +1,24 @@
-export default function Contact(){
-    return (
-        <main className="flex justify-center space-x-32 text-xl">
-            <a href="https://twitter.com/gregarijah">Twitter</a>
-            <a href="https://github.com/gregArijah">GitHub</a>
-            <a href="mailto:gregarijah@gmail.com">Email</a>
-        </main>
-    )
+import { twitterIcon, linkedinIcon, githubIcon, emailIcon } from '../icons';
+
+export default function Contact() {
+  return (
+    <main className='flex space-x-24 justify-center'>
+        <hr />
+      
+        <a href="https://twitter.com/gregarijah" className="flex h-8 text-xl space-x-2" >
+            {twitterIcon} <span>Twitter</span> 
+        </a>
+        <a href="https://github.com/gregArijah" className="flex h-8 text-xl space-x-2">
+            {githubIcon} <span>GitHub</span>
+        </a>
+        <a href="https://www.linkedin.com/in/arijah1/" className="flex h-8 text-xl space-x-2">
+            {linkedinIcon} <span>LinkedIn</span>
+        </a>
+        <a href="mailto:gregarijah@gmail.com" className="flex h-8 text-xl space-x-2">
+            {emailIcon} <span>E-mail</span>
+        </a>
+       
+
+    </main>
+  );
 }
--- app/icons.tsx
+++ app/icons.tsx
@@ -1,10 +1,8 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronDown, faTrashCan, faFileCirclePlus, faFolderOpen, faCloudArrowDown, 
-         faComments, faScrewdriverWrench, faCircleQuestion, faDoorOpen, faBuildingColumns, 
-         faList, faCode, faDatabase, faRobot, faCirclePlus, faCircleRight, faPenToSquare,
-        faWarehouse} from '@fortawesome/free-solid-svg-icons';
+import { faTwitter, faLinkedinIn, faGithub} from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
-export const circleRightIcon = <FontAwesomeIcon icon={faCircleRight} />
-export const trashIcon = <FontAwesomeIcon icon={faTrashCan} style={{color: "#ff6600",}} />
-export const dropIcon = <FontAwesomeIcon icon={faChevronDown} style={{color: "#ff6600",}} />
-export const newIcon = <FontAwesomeIcon icon={faFileCirclePlus} style={{color: "#ff6600",}} />
+export const twitterIcon = <FontAwesomeIcon icon={faTwitter } />
+export const linkedinIcon = <FontAwesomeIcon icon={faLinkedinIn} />
+export const githubIcon = <FontAwesomeIcon icon={faGithub} />
+export const emailIcon = <FontAwesomeIcon icon={faEnvelope} />
