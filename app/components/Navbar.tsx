5ecb8230e486ca9075571c5a320d49bd334e8a2d
Greg Griffith <griffith_g@hotmail.com>
2023-06-05 17:30:09 -0400

    edit navbar for mobile

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,14 +3,11 @@
 export default function Navbar() {
 
     return (
-        <main className="p-5 flex w-screen justify-end sticky top-0 bg-gray-300" id="home">
-            <div className="space-x-6 text-l pr-5">
-                <a href="#home">home</a>
+        <main className="flex flex-row space-x-3 p-2 w-screen text-sm justify-end fixed top-0 bg-zinc-300">
                 <a href="#about">about_me</a>
                 <a href="#tech">tech_stack</a> 
                 <a href="#projects">projects</a>
                 <a href="#contact">contact_me</a>
-            </div>
         </main>
     )
 }
