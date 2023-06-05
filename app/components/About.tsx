c9765f263f006f05a8bf23f376dc54758bb28135
Greg Griffith <griffith_g@hotmail.com>
2023-06-05 17:57:46 -0400

    edit about for mobile

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,11 +1,9 @@
 export default function About() {
     return (
-        <main className="flex justify-between p-10 pl-20 pr-20" id="about">
-            <div className="space-y-2">
-                <p className="text-xl font-bold pb-2">A little about myself</p>
-                <p className="text-xl">I am a software developer with a passion for engineering and problem solving. I love building innovative solutions, and creating cool projects with real applications. I have a background in manufacturing technology and I am very excited to apply my newly honed skills to old and upcoming challenges alike.</p>
-            </div>
-        
+        <main className="flex flex-col justify-between p-5 space-y-2" id="about">
+                <p className="text-md font-bold">A little about myself</p>
+                <p className="text-sm">I am a software developer with a passion for engineering and problem solving. I enjoy building innovative solutions, and creating cool projects with real applications. I have a background in manufacturing technology and I am very excited to apply my newly honed skills to old and upcoming challenges alike.</p>
+
         </main>   
     )
 }
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,9 +6,9 @@ export default function Header() {
     return (
         <main className="flex justify-between p-5" >
             <div className="flex flex-col justify-center">
-                <p className="text-lg font-bold pb-2 pt-8">Greg Arijah</p>
-                <p className="text-xs">Software Developer</p>
-                <p className="text-xs">Toronto, Canada</p>
+                <p className="text-lg font-bold pb-1 pt-8">Greg Arijah</p>
+                <p className="text-sm">Software Developer</p>
+                <p className="text-sm">Toronto, Canada</p>
             </div>
             <div>
                 <Image src="/images/avatar.png" width={200} height={200} alt="hello world"/>
