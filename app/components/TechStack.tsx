97867fb870b9b5c8041876845f4dff8f2f2833ae
Greg Griffith <griffith_g@hotmail.com>
2023-06-09 21:14:52 -0400

    updated space to gap in grid layout

--- app/components/TechStack.tsx
+++ app/components/TechStack.tsx
@@ -7,7 +7,7 @@ export default function TechStack() {
         <main className="flex flex-col justify-between p-5 space-y-2" id="tech">
                 <p className="text-md font-bold">Tech Stack</p>
                 <p className="text-sm">I have experience with the following technologies:</p>
-                <div className="grid grid-cols-4 md:grid-cols-6 lg:grid-cols-12 text-sm space-x-2 space-y-2">
+                <div className="grid grid-cols-4 md:grid-cols-6 lg:grid-cols-12 text-sm gap-x-2 gap-y-2">
                     <Image src="/images/techstack/005-nodejs.png" height="64" width="64" alt=""/>
                     <Image src="/images/techstack/expressjs.png" height="64" width="64" alt=""/>
                     <Image src="/images/techstack/nextjs.jpg" height="64" width="64" alt=""/>
