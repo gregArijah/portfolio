32c1131f384c328c60f403a58140f9f2b134c177
Greg Griffith <griffith_g@hotmail.com>
2023-06-17 18:56:00 -0400

    update href title attributes in contact section

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -3,10 +3,10 @@ import { twitterIcon, linkedinIcon, githubIcon, emailIcon } from '../icons';
 export default function Contact() {
   return (
     <main id="contact" className='flex space-x-8 justify-center h-8'>
-            <a href="https://twitter.com/gregarijah" className="flex h-6" >{twitterIcon} </a>
-            <a href="https://github.com/gregArijah" className="flex h-6">{githubIcon} </a>
-            <a href="https://www.linkedin.com/in/arijah1/" className="flex h-6">{linkedinIcon} </a>
-            <a href="mailto:gregarijah@gmail.com" className="flex h-6">{emailIcon}</a>
+            <a href="https://twitter.com/gregarijah" className="flex h-6" title="Contact me on Twitter">{twitterIcon} </a>
+            <a href="https://github.com/gregArijah" className="flex h-6"  title="Visit my Github">{githubIcon} </a>
+            <a href="https://www.linkedin.com/in/arijah1/" className="flex h-6"  title="Contact me on LikedIn">{linkedinIcon} </a>
+            <a href="mailto:gregarijah@gmail.com" className="flex h-6"  title="Contact me on Twitter">{emailIcon}</a>
     </main>
   );
 }
