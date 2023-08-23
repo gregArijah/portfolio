92a07be394adfd264d76642193678097fc6eb37d
Greg Griffith <griffith_g@hotmail.com>
2023-08-23 00:27:21 -0400

    updating project section

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -5,24 +5,32 @@ export default function Projects() {
     let projects = [ 
                 {
                     name: "Javatrol",
-                    description: "is a web based conversational CNC programming tool developed by machinists for machinists. From its inception it was designed to be user friendly, intuitive and provide all the coulds that \"if only this program could...\"",
+                    description: "is a full stack conversational CNC programming tool developed by machinists for machinists. From its inception it was designed to be user friendly, intuitive and provide all the coulds that \"if only this program could...\"",
                     image: "/images/projects/javatrol.jpeg",
                     link: "https://javatrol.herokuapp.com",
                     tech: ["Javascript","React", "MongoDB", "Express", "Node.js", "HeadlessUI", "TailwindCSS"],
                 },
                 {
                     name: "Veleron",
-                    description: "is a web based attendance and time tracking app; a connected punchclock. The initial release of the app will focus mainly on calculating the number of hours a given employee has worked over a selected period of time.",
+                    description: "is a full stack attendance and time tracking app; a connected punchclock. The initial release will focus on calculating the number of hours employees have worked over a selected period in order to calculate their pay.",
                     image: "/images/projects/veleron8.png",
                     link: "https://veleron.vercel.app/",
                     tech: ["Typescript", "Next.js", "Prisma", "PostgreSQL", "Node.js","NextAuth.js", "MaterialUI", "TailwindCSS"],
                 },
                 {
                     name: "MotoMetrix",
-                    description: "is an arduino project which collects data from sensors on a vehicle and displays it to a LCD screen. During the pandemic I decided to learn to weld, after successfully building a go-kart I built a digital dashboard using the arduino-nano controller.",
+                    description: "is a dashboard for recreational vehicles; an arduino project which collects and displays sensor data. During the pandemic I learned to weld, and after a successful go-kart build, I built a dashboard using the arduino controller.",
                     image: "/images/projects/comingsoon.jpeg",
                     link: "https://github.com/gregArijah/digital-dashboard/",
                     tech: ["C++", "Arduino"]
+                },
+                {
+                    name: "EVie",
+                    description: "is a web based application that allows users to search for electric vehicle charging stations across North America. The app uses the NREL API to find charging stations and the Google Maps API to display them on a map.",
+                    image: "/images/projects/evie.jpeg",
+                    link: "https://yerimechoi.github.io/eVie/",
+                    tech: ["Javascript", "jQuery", "HTML", "CSS",  "Materialize", "NREL API", "Google Maps API"],
+
                 },
             ];
 
@@ -77,8 +85,9 @@ export default function Projects() {
                 {projects.map((project, index) => {
                     return ( 
                         <div  key ={index} className="w-full space-y-1">
-                            <div className="flex flex-row">
-                                <p className="text-sm"><span className="font-bold">{project.name}</span> {project.description}</p>
+                            <p className="font-bold">{project.name}</p>
+                            <div className="flex flex-row">    
+                                <p className="text-sm"><span>{project.name}</span> {project.description}</p>
                                 <p><a href={project.link}><Image src={project.image} className="rounded" height="600" width="600" alt="projects"/></a></p>
                             </div>
                             <p className="pb-6"> 
@@ -92,9 +101,15 @@ export default function Projects() {
                 })}
             
                 <p className="text-sm font-bold">Challenges and Assignments from Bootcamp</p>
-                <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 text-xs gap-x-2 gap-y-2">
+                {/* <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 text-xs gap-x-2 gap-y-2"> */}
+                <div className="grid grid-cols-2 md:grid-cols-4 lg:hidden text-xs gap-x-2 gap-y-2">
                     {bootcamp.map((project, index) => {
                         return ( <a key={index} className="flex flex-col justify-between"href={project.link}><span className="font-normal">{project.name}</span><Image src={project.image} className="w-auto h-auto border-2 border-slate-300" height="350" width="350" alt="projects"/></a>)
+                    })}
+                </div>    
+                <div className="hidden lg:flex flex-col text-xs gap-x-2 gap-y-2">
+                    {bootcamp.map((project, index) => {
+                        return ( <a key={index} className="flex flex-col justify-between"href={project.link}><span className="font-normal">{project.name}</span><Image src={project.image} className=" border-2 border-slate-300" height="75" width="85" alt="projects"/></a>)
                     })}
                 </div>    
         </main>
