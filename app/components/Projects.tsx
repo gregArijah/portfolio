9ed9a3874f2ec843861725a9b0844ecc807fb1d5
Greg Griffith <griffith_g@hotmail.com>
2023-08-23 01:20:02 -0400

    updating project section

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,4 +1,7 @@
 import Image from "next/image"
+import Link from "next/link"
+import { internetIcon, githubIcon } from "../icons"
+
 
 export default function Projects() {
 
@@ -8,6 +11,7 @@ export default function Projects() {
                     description: "is a full stack conversational CNC programming tool developed by machinists for machinists. From its inception it was designed to be user friendly, intuitive and provide all the coulds that \"if only this program could...\"",
                     image: "/images/projects/javatrol.jpeg",
                     link: "https://javatrol.herokuapp.com",
+                    github: "https://github.com/gregArijah/cnc-gcode-generator",
                     tech: ["Javascript","React", "MongoDB", "Express", "Node.js", "HeadlessUI", "TailwindCSS"],
                 },
                 {
@@ -15,6 +19,7 @@ export default function Projects() {
                     description: "is a full stack attendance and time tracking app; a connected punchclock. The initial release will focus on calculating the number of hours employees have worked over a selected period in order to calculate their pay.",
                     image: "/images/projects/veleron8.png",
                     link: "https://veleron.vercel.app/",
+                    github: "https://github.com/gregArijah/workforce-manager",
                     tech: ["Typescript", "Next.js", "Prisma", "PostgreSQL", "Node.js","NextAuth.js", "MaterialUI", "TailwindCSS"],
                 },
                 {
@@ -22,6 +27,7 @@ export default function Projects() {
                     description: "is a dashboard for recreational vehicles; an arduino project which collects and displays sensor data. During the pandemic I learned to weld, and after a successful go-kart build, I built a dashboard using the arduino controller.",
                     image: "/images/projects/comingsoon.jpeg",
                     link: "https://github.com/gregArijah/digital-dashboard/",
+                    github: "https://github.com/gregArijah/digital-dashboard/",
                     tech: ["C++", "Arduino"]
                 },
                 {
@@ -29,6 +35,7 @@ export default function Projects() {
                     description: "is a web based application that allows users to search for electric vehicle charging stations across North America. The app uses the NREL API to find charging stations and the Google Maps API to display them on a map.",
                     image: "/images/projects/evie.jpeg",
                     link: "https://yerimechoi.github.io/eVie/",
+                    github: "https://github.com/yerimechoi/eVie",
                     tech: ["Javascript", "jQuery", "HTML", "CSS",  "Materialize", "NREL API", "Google Maps API"],
 
                 },
@@ -85,10 +92,11 @@ export default function Projects() {
                 {projects.map((project, index) => {
                     return ( 
                         <div  key ={index} className="w-full space-y-1">
-                            <p className="font-bold">{project.name}</p>
-                            <div className="flex flex-row">    
-                                <p className="text-sm"><span>{project.name}</span> {project.description}</p>
-                                <p><a href={project.link}><Image src={project.image} className="rounded" height="600" width="600" alt="projects"/></a></p>
+                            <p className="font-bold space-x-2"><span>{project.name}</span> <span><Link href={project.link}>{internetIcon} </Link><Link href={project.github}>{githubIcon}</Link></span></p>
+                            <div className="flex flex-col sm:flex-row">    
+                                <p className="text-sm">{project.name} {project.description}</p>
+                                <p className="hidden sm:flex"><Image src={project.image} className="rounded" height="600" width="600" alt="projects"/></p>
+                                <p className="sm:hidden"><Image src={project.image} className="rounded" height="350" width="350" alt="projects"/></p>
                             </div>
                             <p className="pb-6"> 
                                 {project.tech && project.tech.map((tech, index) => {
--- app/icons.tsx
+++ app/icons.tsx
@@ -1,8 +1,11 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitter, faLinkedinIn, faGithub} from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faTwitter, faLinkedinIn, faGithub, faGithubAlt} from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faGlobe } from '@fortawesome/free-solid-svg-icons';
 
 export const twitterIcon = <FontAwesomeIcon icon={faTwitter } />
 export const linkedinIcon = <FontAwesomeIcon icon={faLinkedinIn} />
 export const githubIcon = <FontAwesomeIcon icon={faGithub} />
+export const githubIcon2 = <FontAwesomeIcon icon={faGithubAlt} />
 export const emailIcon = <FontAwesomeIcon icon={faEnvelope} />
+export const internetIcon = <FontAwesomeIcon icon={faGlobe} />
+
