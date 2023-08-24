1a4331bc8f1f804b829c714dbb3fc03e87c92eaf
Greg Griffith <griffith_g@hotmail.com>
2023-08-24 01:36:02 -0400

    updating tech icons sizing

--- app/components/TechStack.tsx
+++ app/components/TechStack.tsx
@@ -7,7 +7,21 @@ export default function TechStack() {
         <main className="flex flex-col justify-between p-5 space-y-2" id="tech">
                 <p className="text-md font-bold">Tech Stack</p>
                 <p className="text-sm">I have experience with the following technologies:</p>
-                <div className="grid grid-cols-6 md:grid-cols-12 lg:grid-cols-12 text-sm gap-x-4 sm:gap-x-2 gap-y-2 w-11/12 sm:w-full">
+                <div className="grid grid-cols-6 md:grid-cols-12 lg:grid-cols-12 text-sm gap-x-4 sm:gap-x-2 gap-y-2 w-11/12 sm:w-full md:hidden">
+                    <Image src="/images/techstack/005-nodejs.png" height="48" width="48" alt=""/>
+                    <Image src="/images/techstack/expressjs.png" height="48" width="48" alt=""/>
+                    <Image src="/images/techstack/nextjs.jpg" height="48" width="48" alt=""/>
+                    <Image src="/images/techstack/007-physics.png" height="48" width="48" alt=""/>
+                    <Image src="/images/techstack/004-typescript.png" height="48" width="48" alt=""/>
+                    <Image src="/images/techstack/006-js.png" height="48" width="48" alt=""/>
+                    <Image src="/images/techstack/mongodb.png" height="48" width="48" alt=""/>
+                    <Image src="/images/techstack/008-mysql.png" height="48" width="48" alt=""/>
+                    <Image src="/images/techstack/003-postgre.png" height="48" width="48" alt=""/>
+                    <Image src="/images/techstack/graphql.png" height="48" width="48" alt=""/>
+                    <Image src="/images/techstack/tailwind.png" height="48" width="48" alt=""/>
+                    <Image src="/images/techstack/arduino.png" height="48" width="48" alt=""/>
+                </div>
+                <div className="md:grid grid-cols-6 md:grid-cols-12 lg:grid-cols-12 text-sm gap-x-4 sm:gap-x-2 gap-y-2 w-11/12 sm:w-full hidden">
                     <Image src="/images/techstack/005-nodejs.png" height="64" width="64" alt=""/>
                     <Image src="/images/techstack/expressjs.png" height="64" width="64" alt=""/>
                     <Image src="/images/techstack/nextjs.jpg" height="64" width="64" alt=""/>
