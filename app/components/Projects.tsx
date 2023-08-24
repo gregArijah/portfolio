eceba80e71d3d2c305dfd3959075c6a4e43c7c18
Greg Griffith <griffith_g@hotmail.com>
2023-08-23 22:22:41 -0400

    updating project section ui

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -24,7 +24,7 @@ export default function Projects() {
                 },
                 {
                     name: "MotoMetrix",
-                    description: "is a dashboard for recreational vehicles; an arduino project which collects and displays sensor data. During the pandemic I learned to weld, and after a successful go-kart build, I built a dashboard using the arduino controller.",
+                    description: "is a digital dashboard; an arduino project which collects and displays sensor data. During the pandemic I learned to weld, and after a successful go-kart build, I built a dashboard using the arduino controller.",
                     image: "/images/projects/comingsoon.jpeg",
                     link: "https://github.com/gregArijah/digital-dashboard/",
                     github: "https://github.com/gregArijah/digital-dashboard/",
@@ -44,43 +44,48 @@ export default function Projects() {
     let bootcamp = [
                 {
                     name: "Emperor's Dish Restaurant",
+                    description: "A full stack restaurant app that allows users to view the menu, place orders. It also allows the restaurant to manage their menu information.",
                     image: "/images/projects/emperors_dish.png",
-                    link: "https://emperors-dish-app.herokuapp.com/"
-                },
-                {
-                    name: "EVie - Charging Station Finder",
-                    image: "/images/projects/evie.jpeg",
-                    link: "https://yerimechoi.github.io/eVie/"
+                    link: "https://emperors-dish-app.herokuapp.com/",
+                    github: ""
                 },
                 {
                     name: "Social Network API",
+                    description: "An SQL back end for a social network web application where users can share their thoughts, react to friends’ thoughts, and create a friend list.",
                     image: "/images/projects/social_network_api.jpeg",
                     link: "https://drive.google.com/file/d/1RoycjaPsk8E1S74Ppugd6MisYlxK5ODc/view"
                 },
                 {
                     name: "E-Commerce Backend",
+                    description: "A noSQL back end for an e-commerce site to track and perform CRUD operations on products, categories, and tags. ",
                     image: "/images/projects/ecommerce_backend.jpeg",
                     link: "https://drive.google.com/file/d/1GUHcmWtSXxZi5MqFKBizJhMTyS2hSbhb/view"
                 },
                 {
                     name: "Weather Dashboard",
+                    description: "A weather dashboard that runs in the browser and features dynamically updated HTML and CSS.",
                     image: "/images/projects/weather_dashboard.jpeg",
                     link: "https://gregarijah.github.io/challenge-6-weather-dashboard/"
                 },
                 {
                     name: "Team Profile Generator",
+                    description: "A Node.js command-line application that takes in information about employees on a team, then generates an HTML webpage that displays summaries for each person.",
                     image: "/images/projects/team_profile.jpeg",
                     link: "https://drive.google.com/file/d/1LF9vyzf8_zUaKiBWMi5ACeH7eV0KrdZE/view"
                 },
                 {
                     name: "JavaScript Challenge",
+                    description: "A timed coding quiz with multiple-choice questions. This app runs in the browser and features dynamically updated HTML and CSS powered by JavaScript code.",
                     image: "/images/projects/coding_quiz.jpeg",
-                    link: "https://gregarijah.github.io/challenge-4-javascript-quiz/"
+                    link: "https://gregarijah.github.io/challenge-4-javascript-quiz/",
+                
                 },
                 {
                     name: "Password Generator",
+                    description: "An application that generates a random password based on user-selected criteria. This app runs in the browser and features dynamically updated HTML and CSS powered by JavaScript code.",
                     image: "/images/projects/password_generator.jpeg",
-                    link: "https://gregarijah.github.io/challenge-3-password-generator/develop/"
+                    link: "https://gregarijah.github.io/challenge-3-password-generator/develop/",
+                    github: "https://github.com/gregArijah/challenge-3-password-generator"    
                 },
             ];
 
@@ -91,35 +96,31 @@ export default function Projects() {
                     
                 {projects.map((project, index) => {
                     return ( 
-                        <div  key ={index} className="w-full space-y-1">
-                            <p className="font-bold space-x-2"><span>{project.name}</span> <span><Link href={project.link}>{internetIcon} </Link><Link href={project.github}>{githubIcon}</Link></span></p>
-                            <div className="flex flex-col sm:flex-row">    
-                                <p className="text-sm text-justify mr-3">{project.name} {project.description}</p>
-                                <p className="hidden sm:flex border border-black rounded"><Image src={project.image} className="rounded" height="600" width="600" alt="projects"/></p>
-                                <p className="sm:hidden border border-black rounded"><Image src={project.image} className="rounded" height="350" width="350" alt="projects"/></p>
-                            </div>
-                            <p className="pb-6"> 
-                                {project.tech && project.tech.map((tech, index) => {
-                                    return (<span key={index} className="text-xs font-bold bg-blue-900 rounded-md p-1 mr-1">{tech} </span>)
-                                })}
+                        <div  key ={index} className="w-full space-y-1 pb-8">
+                            <p className="font-bold space-x-3">
+                                <span>{project.name}</span> 
+                                <span><Link href={project.link}>{internetIcon}</Link></span>
+                                <span><Link href={project.github}>{githubIcon}</Link></span>
                             </p>
+                            <div className="flex flex-col sm:flex-row space-y-2">    
+                                <div className="flex flex-col space-y-2">
+                                    <p className="text-sm text-justify mr-3">{project.name} {project.description}</p>
+                                    <p>
+                                        {project.tech.map((tech, index) => {
+                                            return (<span key={index} className="text-xs font-bold bg-blue-900 rounded-md p-1 mr-1">{tech} </span>)
+                                        })}
+                                    </p>
+                                </div>
+                                <p className="h-min hidden sm:flex border border-black rounded"><Image src={project.image} className="rounded" height="600" width="600" alt="projects"/></p>
+                                <p className="sm:hidden max-w-max border border-black rounded"><Image src={project.image} className="rounded" height="350" width="350" alt="projects"/></p>
+                            </div>
+                            
                             
                         </div>
                         )
                 })}
             
-                <p className="text-sm font-bold">Challenges and Assignments from Bootcamp</p>
-                {/* <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-8 text-xs gap-x-2 gap-y-2"> */}
-                <div className="grid grid-cols-2 md:grid-cols-4 lg:hidden text-xs gap-x-2 gap-y-2">
-                    {bootcamp.map((project, index) => {
-                        return ( <a key={index} className="flex flex-col justify-between"href={project.link}><span className="font-normal">{project.name}</span><Image src={project.image} className="w-auto h-auto border-2 border-slate-300" height="350" width="350" alt="projects"/></a>)
-                    })}
-                </div>    
-                <div className="hidden lg:flex flex-col text-xs gap-x-2 gap-y-2">
-                    {bootcamp.map((project, index) => {
-                        return ( <a key={index} className="flex flex-col justify-between"href={project.link}><span className="font-normal">{project.name}</span><Image src={project.image} className=" border-2 border-slate-300" height="75" width="85" alt="projects"/></a>)
-                    })}
-                </div>    
+                
         </main>
     )
 
