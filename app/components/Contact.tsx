9e0181c0c0a5a79ede8b09a674b18d38caa4cf1a
Greg Griffith <griffith_g@hotmail.com>
2023-09-03 13:03:59 -0400

    updating links in contact section

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -4,16 +4,16 @@ export default function Contact() {
   return (
     <div>
         <div id="contact" className='flex space-x-8 justify-center h-8 lg:hidden'>
-                <a href="https://twitter.com/gregarijah" className="flex h-6" title="Contact me on Twitter">{twitterIcon} </a>
-                <a href="https://github.com/gregArijah" className="flex h-6"  title="Visit my Github">{githubIcon} </a>
-                <a href="https://www.linkedin.com/in/arijah1/" className="flex h-6"  title="Contact me on LikedIn">{linkedinIcon} </a>
-                <a href="mailto:gregarijah@gmail.com" className="flex h-6"  title="Send me an email">{emailIcon}</a>
+                <a  target="_blank" href="https://twitter.com/gregarijah" className="flex h-6" title="Contact me on Twitter">{twitterIcon} </a>
+                <a target="_blank" href="https://github.com/gregArijah" className="flex h-6"  title="Visit my Github">{githubIcon} </a>
+                <a target="_blank" href="https://www.linkedin.com/in/arijah1/" className="flex h-6"  title="Contact me on LikedIn">{linkedinIcon} </a>
+                <a target="_blank" href="mailto:gregarijah@gmail.com" className="flex h-6"  title="Send me an email">{emailIcon}</a>
         </div>
         <div id="contact" className='hidden lg:flex space-x-8 justify-center fixed bottom-28 pl-5'>
-                <a href="https://twitter.com/gregarijah" className="flex h-6 text-2xl" title="Contact me on Twitter">{twitterIcon} </a>
-                <a href="https://github.com/gregArijah" className="flex h-6 text-2xl"  title="Visit my Github">{githubIcon} </a>
-                <a href="https://www.linkedin.com/in/arijah1/" className="flex h-6 text-2xl"  title="Contact me on LikedIn">{linkedinIcon} </a>
-                <a href="mailto:gregarijah@gmail.com" className="flex h-6 text-2xl"  title="Send me an email">{emailIcon}</a>
+                <a target="_blank" href="https://twitter.com/gregarijah" className="flex h-6 text-2xl" title="Contact me on Twitter">{twitterIcon} </a>
+                <a target="_blank" href="https://github.com/gregArijah" className="flex h-6 text-2xl"  title="Visit my Github">{githubIcon} </a>
+                <a target="_blank" href="https://www.linkedin.com/in/arijah1/" className="flex h-6 text-2xl"  title="Contact me on LikedIn">{linkedinIcon} </a>
+                <a target="_blank" href="mailto:gregarijah@gmail.com" className="flex h-6 text-2xl"  title="Send me an email">{emailIcon}</a>
         </div>
     </div>
   );
