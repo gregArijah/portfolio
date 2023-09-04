cacdfa1da76b4b9625edcbe3160f687bac936475
Greg Griffith <griffith_g@hotmail.com>
2023-09-04 14:06:28 -0400

    update about me

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -2,8 +2,8 @@ export default function About() {
     return (
         <div className="flex flex-col justify-between p-5 space-y-2" id="about">
                 <p className="text-lg font-bold">About Me</p>
-                <p className="text-sm text-justify">I am a software developer with a passion for engineering and a knack for simplifying complex tasks. With a background in CNC machining and programming, over the years I have developed strong analytical and problem solving skills, and the ability to come up with effective solutions both in and outside the box. I feel most accomplished when I create solutions that make processess more efficient either by eliminating and centralizing operations through code or by analyzing operations, identifying patterns and creating parameterized code usuable for a range of input values.</p>
-                <p className="text-sm text-justify">Thank you for taking an interest in my work and for taking the time to review my portfolio. </p>
+                <p className="text-sm text-justify">I am a software developer with a passion for engineering and a knack for simplifying complex tasks. I have a professional background in CNC programming and machining, and over the years I have developed strong analytical and problem solving skills, and the ability to come up with effective solutions both in and outside the box. I feel most accomplished when I create solutions that make processes more efficient either by eliminating and centralizing operations through code as with the Veleron project or by analyzing operations, identifying patterns and creating parameterized code usuable for a range of input values as with the Javatrol project.</p>
+                <p className="text-sm text-justify"> Thank you for taking an interest in my work and for taking the time to review my portfolio. Let's connect and discuss how my skills and experience can contribute to your team's success. </p>
         </div>   
     )
 }
